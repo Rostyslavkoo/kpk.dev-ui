cbdc859b5c89ace93e17a09fa70f63473a897149
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2022-02-12 23:35:56 +0200

    dist

---
+++ js/chunk-2d209259.e573474b.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d209259"],{a88d:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("RouterView")},c=[],l={},r=l,a=t("2877"),o=Object(a["a"])(r,u,c,!1,null,null,null);n["default"]=o.exports}}]);
